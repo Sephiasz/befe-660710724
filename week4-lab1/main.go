39e20f1f20d04ca2ca97eea5da3c77205d70376c
Onochayama <143159977+Sephiasz@users.noreply.github.com>
2025-08-06 03:33:48 +0000

    commit

---
+++ week4-lab1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import {
+    "fmt"
+}
+// var gmail String = "Suebyart_p@su.ac.th"
+func main() {
+    var name String = "Pongrapee"
+	var age int = 20
+
+	email := "Suebyart_p@su.ac.th"
+	gpa := 3.17
+
+	firstname,lastname := "Pongrapee","Suebyart"
+
+	fmt.Printf("Name %s %s, age %d, gmail %s, gpa %.2f\n",firstname,lastname,age,gmail,gpa)
+}
